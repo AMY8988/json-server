af3955a3cfeaa693ec0b99313e0efcda6407c3c8
AMY8988 <amymoe2321@gmail.com>
2022-07-06 20:02:16 +0630

    testing-1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class= " container hello">
+    <div class="row align-items-start">
+      <div v-if="lists.length == 0" class="col-12">
+        <h3 class=" text-black-50"> News admin Dashboard with vuejs</h3>
+      </div>
+      <div v-else v-for="list in lists" :key="list.id"  class=" col-3  my-5">
+            <div class="text-start border border-0 shadow p-3 rounded">
+              <h4 class=" text-center fw-bolder"> {{list.title}} </h4>
+              <p class=" text-center text-black-50"> {{list.description}} </p>
+              <span class=" text-black ">- {{list.author}} </span>
+            </div>
+          </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: "",
+      description: "",
+      author: "",
+      lists: []
+    }
+  },
+  mounted(){
+      
+        axios.get('http://localhost:3000/posts')
+        .then(response => {
+          console.log(response.data)
+          this.lists = response.data
+        
+        })
+      
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class=" container about">
+    <div class="row">
+      <div class="col-12">
+        <h1>Edit Json Server</h1>
+        <div class="d-flex  flex-column align-items-center">
+          <div class="col-5">
+          <div class=" ">
+              <input v-model="title" placeholder="Title" type="text" class=" form-control my-2">
+              <textarea v-model="description" placeholder=" description" name="" class=" my-2 form-control" id="" cols="50" rows="10"></textarea>  
+              <div class=" col-4 my-2">
+                <input v-model="author" placeholder="author" type="text" class=" form-control">
+                
+              </div>
+              <button @click="addPost()" class=" btn btn-outline-primary rounded ">Submit</button>
+              
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</template>
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+
+      title: "",
+      description: "",
+      author: "",
+      
+    }
+  },
+  methods: {
+    addPost() {
+      let newdata = {
+        title : this.title,
+        description : this.description,
+        author : this.author
+      };
+      console.log(newdata)
+      
+
+      
+
+      if(this.title == ""){
+        alert("can't add newpost")
+      }else{
+        axios.post("http://localhost:3000/posts" , newdata);
+        this.title = "";
+        this.description ="";
+        this.author = "";
+      }
+      
+        
+        
+      
+    }
+    
+  }
+  
+}
+</script>
